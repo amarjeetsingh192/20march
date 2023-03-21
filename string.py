d0f1d6cf4bdee4ac98c04e0e5321059c78a14b2e
amarjeetsingh192 <125898800+amarjeetsingh192@users.noreply.github.com>
2023-03-21 15:30:07 +0530

    Add files via upload
    initial

---
+++ string.py
@@ -0,0 +1,25 @@
+a=10
+print(type(a))
+print(id(a))
+
+#program2
+a="hello"
+print(type(a))
+print(id(a))
+
+#program 3
+a=[1,2,3,4,5]
+print(type(a))
+
+
+#program 4
+
+str1="hello"
+print(id(str1))
+
+str2="world"
+print(id(str1))
+
+str1=str1+str2
+print(str1)
+print(id(str1))
---
+++ string2.py
@@ -0,0 +1,13 @@
+s="python"
+
+print(s[1:6:1])
+print(s[3])
+a=0
+for i in s:
+  print("s[%a]:" %i,a)
+  a+=1
+
+a=10
+b=2
+c=a^b
+print(c)
